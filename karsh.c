44f12acbf98f90a27d8886442594e01530c07f1b
K4rthik14 <karthiksaya14@gmail.com>
2026-02-12 21:45:01 +0530

    fixed the whole code

--- karsh.c
+++ karsh.c
@@ -1,65 +1,58 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include<unistd.h>
-#include<sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 
-int main()
-{
-	char input[MAX_LINE];
-	char *args[MAX_ARGS];
+int main() {
+    char input[MAX_LINE];
+    char *args[MAX_ARGS];
 
-	while(1)
-	{
-		//print prompt
-		printf("karsh>");
-		fflush(stdout);
-		
-		//To read input
-		if(fgets(input,MAX_LINE, stdin)==NULL)
-		{
-			break;
-		}
-		
-		//To Remove newline
-		input[strcspn(input,"\n")]==0;
+    while (1) {
+        // Print prompt
+        printf("karsh> ");
+        fflush(stdout);
 
-		//Exit Builtin
-		if(strcmp(input,"exit")==0)
-			printf("Exiting karsh ....\n");
-			break;
-	}
-	//parse input into arguments
-	int i = 0;
-	args[i] = strtok(input, " ");
-	while(args[i] != NULL)
-	{
-		i++;
-		args[i]= strtok(NULL, " ");
+        // Read input
+        if (fgets(input, MAX_LINE, stdin) == NULL) {
+            break;
+        }
 
-	}
-	//Fork Process
-	pid_t pid = fork();
+        // Remove newline
+        input[strcspn(input, "\n")] = 0;
 
-	if(pid == 0 )
-	{
-		//child process
-		execvp(args[0],args);
-		perror("exec failed");
-		exit(1);
+        // Exit builtin
+        if (strcmp(input, "exit") == 0) {
+            printf("Exiting karsh...\n");
+            break;
+        }
 
-	}
-	else if (pid > 0)
-	{	
-		//for parent process
-		wait(NULL);
-	}else
-	{
-		perror("fork failed");
+        // Parse input into arguments
+        int i = 0;
+        args[i] = strtok(input, " ");
+        while (args[i] != NULL) {
+            i++;
+            args[i] = strtok(NULL, " ");
+        }
 
-	}
-	return 0;
+        // Fork process
+        pid_t pid = fork();
+
+        if (pid == 0) {
+            // Child process
+            execvp(args[0], args);
+            perror("exec failed");
+            exit(1);
+        } else if (pid > 0) {
+            // Parent process
+            wait(NULL);
+        } else {
+            perror("fork failed");
+        }
+    }
+
+    return 0;
 }

22fc3527fd550b6280f1e4ae3906f7e4db69fce5
K4rthik14 <karthiksaya14@gmail.com>
2026-02-13 17:13:31 +0530

    added the explain mode

--- karsh.c
+++ karsh.c
@@ -7,6 +7,8 @@
 #define MAX_LINE 1024
 #define MAX_ARGS 64
 
+void explain_command(char *topic);
+
 int main() {
     char input[MAX_LINE];
     char *args[MAX_ARGS];
@@ -29,6 +31,13 @@ int main() {
             printf("Exiting karsh...\n");
             break;
         }
+
+	//Explain mode builtin
+	if(strcmp(input, "explain", 8) == 0)
+	{	
+		explain_command(input + 8);
+		countinue;
+	}
 
         // Parse input into arguments
         int i = 0;
@@ -55,4 +64,31 @@ int main() {
     }
 
     return 0;
+}
+
+void explain_command(char *topic) {
+
+    if (strcmp(topic, "fork") == 0) {
+        printf("fork(): creates a child process by duplicating the parent.\n");
+        printf("KARSH uses fork() to create a new process for commands.\n");
+    }
+
+    else if (strcmp(topic, "exec") == 0) {
+        printf("exec(): replaces the current process with a new program.\n");
+        printf("KARSH uses execvp() to run Linux commands.\n");
+    }
+
+    else if (strcmp(topic, "cd") == 0) {
+        printf("cd: changes the current working directory.\n");
+        printf("It must run in the parent process, not a child.\n");
+    }
+
+    else if (strcmp(topic, "scheduler") == 0) {
+        printf("CPU Scheduler decides which process runs next.\n");
+        printf("Examples: FCFS, SJF, Round Robin.\n");
+    }
+
+    else {
+        printf("No explanation available for '%s'\n", topic);
+    }
 }
